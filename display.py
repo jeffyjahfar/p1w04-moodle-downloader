75b66d58e39bfe1f58eb56b421e86a92abb05320
Jeffy Jahfar <jeffyjahfar@gmail.com>
2013-05-23 23:46:19 +0530

    display

--- display.py
+++ display.py
@@ -1,23 +1,42 @@
-#!/usr/bin/env python
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 
+class Example(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        #self.setToolTip('Login with your<b>LDAP</b> username and password')
+        
+        btn = QtGui.QPushButton('Login', self)
+        btn.resize(btn.sizeHint())
+        btn.move(35, 120)
+        
+        qbtn = QtGui.QPushButton('Cancel', self)
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(135, 120)
+        
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Moodle File Downloader')
+        self.setWindowIcon(QtGui.QIcon('moodleicon.png'))        
+    
+        self.show()
+        
 def main():
     
     app = QtGui.QApplication(sys.argv)
-
-    w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Moodle File Downloader')
-    w.show()
-    
+    ex = Example()
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    main()
+    main()    
